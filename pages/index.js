facff7b9cc28b31a2dc170e9850979a2b8addb61
GuiGui Officiel <62442697+GuiGui-OfficielYT@users.noreply.github.com>
2022-02-26 09:06:22 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>GuiGui Officiel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bonjour!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Ceci est un test!
         </p>
       </main>
 
